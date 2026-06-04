04deb449ca28fe9ad244d4e39543fa01b0d5da7f
gopika2204 <150594883+someonefromsomeplace@users.noreply.github.com>
2026-06-04 12:52:52 +0530

    Update sequence_utils.py

--- src/utils/sequence_utils.py
+++ src/utils/sequence_utils.py
@@ -1,1 +1,8 @@
+def validate_dna(seq: str) -> str:
+    """Basic DNA cleaning"""
+    return seq.upper().replace(" ", "").replace("\n", "")
 
+
+def reverse_complement(seq: str) -> str:
+    mapping = str.maketrans("ATGC", "TACG")
+    return seq.translate(mapping)[::-1]
