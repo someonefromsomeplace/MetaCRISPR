4de184db4c17da32db9f615c391eb76943fe4095
gopika2204 <150594883+someonefromsomeplace@users.noreply.github.com>
2026-06-04 12:54:55 +0530

    Update pinn_model.py

--- src/models/pinn_model.py
+++ src/models/pinn_model.py
@@ -1,1 +1,11 @@
+import random
 
+class PINNModel:
+    def predict(self, sequence: str, context: str):
+        return {
+            "specificity": random.uniform(0.5, 0.9),
+            "cfd": random.uniform(0.1, 0.3),
+            "deltaG": random.uniform(-27, -24),
+            "mfe": random.uniform(-3, 0),
+            "gc": random.uniform(0.4, 0.6)
+        }
