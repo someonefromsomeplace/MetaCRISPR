36676490cee8b61446745ad786fa7426dea698af
gopika2204 <150594883+someonefromsomeplace@users.noreply.github.com>
2026-06-04 12:53:13 +0530

    Update dna_preprocessing.py

--- src/data/dna_preprocessing.py
+++ src/data/dna_preprocessing.py
@@ -1,1 +1,16 @@
+from utils.sequence_utils import validate_dna
 
+class DNAProcessor:
+    def process(self, dna_sequence: str):
+        clean_seq = validate_dna(dna_sequence)
+
+        # mock windowing
+        windows = [
+            clean_seq[i:i+100]
+            for i in range(0, len(clean_seq)-100, 50)
+        ]
+
+        return {
+            "validated_sequence": clean_seq,
+            "windows": windows
+        }
